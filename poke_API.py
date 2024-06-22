f8f99bee71b20e0997eb9817a79cd77902177319
Jorge Ariza <v.ariza.0511@gmail.com>
2024-06-22 16:56:20 -0600

    Crear endpoint para api pokedex

---
+++ poke_API.py
@@ -0,0 +1,45 @@
+import requests
+import json
+
+def get_pokemon(url='https://pokeapi.co/api/v2/pokemon-form/',offset = 0):
+
+    args = {'offset' : offset} if offset != 0 else {}
+
+    response = requests.get(url, params = args)
+
+    if response.status_code == 200:
+        # obtener json de API
+        payload = response.json()
+        # obtener atributo results, caso contrario lista vacia
+        result = payload.get('results', [])
+        # result lleno?
+        if result:
+            for pokemon in result:
+                name = pokemon['name']
+                print(name)
+
+        next = input("Continuar listando? [Y/N]").lower()
+        if next == 'y':
+            get_pokemon(offset=offset+20)
+
+if __name__ == '__main__':
+    get_pokemon()
+    '''
+    url = 'https://pokeapi.co/api/v2/pokemon-form/' #lista de pokemons
+
+    #poner limite de resultados a obtener
+    args = {'offset': 0, 'limit': 1000}
+    response = requests.get(url,params=args)
+
+    if response.status_code == 200:
+        #obtener json de API
+        payload = response.json()
+        #obtener atributo results, caso contrario lista vacia
+        result = payload.get('results',[])
+        #result lleno?
+        if result:
+            for pokemon in result:
+                name = pokemon['name']
+                print(name)
+                
+    '''
