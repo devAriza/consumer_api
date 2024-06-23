4f04ae369460e1a1cf5f77e2f0ed21e68607da1a
Jorge Ariza <v.ariza.0511@gmail.com>
2024-06-23 14:46:43 -0600

    Consumir api creada con fastapi con autenticacion OAuth 2.0 y JWT

---
+++ consume_api_fastapi.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+if __name__ == '__main__':
+    username = "jorge12345"
+    password = "12345"
+    data = {
+        'username': username,
+        'password': password,
+    }
+    url = 'http://127.0.0.1:8000/api/v1/auth'
+    response = requests.post(url, data=data)
+    response.raise_for_status()
+
+    # Obtener el token de acceso del cuerpo de la respuesta
+    access_token = response.json()['access_token']
+    print(access_token)
+
+    endpoint = 'http://127.0.0.1:8000/api/v1/users/reviews'
+    headers = {'Authorization': 'Bearer ' + access_token}
+
+    respuesta = requests.get(endpoint, headers=headers)
+    respuesta.raise_for_status()
+
+    # Procesar la respuesta
+    data = respuesta.json()
+    print(data)
